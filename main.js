cd7fce3e4bafe52729352029fd2fd566fa00c0bc
Rallve <lucas.liedgren@gmail.com>
2022-12-06 15:04:36 +0100

    ✨ overhauled the entire game

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import kaboom from "kaboom";
+import { Sound } from "phaser";
 
 // Remember: npx esbuild
 
 kaboom({
-    background:[190, 190, 255]
+    background:[60, 200, 30]
 })
 
 add([
@@ -11,37 +12,76 @@ add([
     pos(120, 180),
 ])
 
-loadSprite("guy", "sprites/guy.png")
 loadSprite("crosshair", "sprites/crosshair.png")
 loadSprite("bullet", "sprites/bullet.png")
+loadSprite("slime", "sprites/slime.png")
+loadSprite("player", "sprites/player.png")
+loadSprite("m4", "sprites/M4.png")
 
-const guy = add([
-    sprite("guy"),
+loadSound("gunshot", "sounds/gunshot.mp3")
+
+volume(0.1)
+
+const m4 = add([
+    sprite("m4"),
+    origin("center"),
     pos(),
-    area(),
-    body(),
+    scale(),
+    rotate(),
+    z(10),
 ])
 
+const player = add([
+    sprite("player"),
+    origin("center"),
+    pos(center()),
+    area(),
+    solid(),
+    scale(0.5),
+    rotate(),
+    z(15),
+])
+
+player.onUpdate(() => {
+    player.angle = crosshair.pos.angle(player.pos)
+    m4.angle = crosshair.pos.angle(m4.pos)
+    m4.moveTo(player.pos.x - 31 * Math.sin(deg2rad(player.angle)), player.pos.y + 31 * Math.cos(deg2rad(player.angle)))
+})
+
+/*
+onKeyPress("f", () => {
+    const slime = add([
+        sprite("slime"),
+        pos(),
+        area(),
+        body(),
+        move(1000, 0),
+    ])
+})
+*/
 const crosshair = add([
     sprite("crosshair"),
     pos(),
+    origin("center"),
     onUpdate(() => {
-        crosshair.moveTo(mousePos().x - 30, mousePos().y - 30)
+        crosshair.moveTo(mousePos().x, mousePos().y)
     })
 ])
 
-onKeyPress("w", () => {
-    if (guy.isGrounded()) {
-        guy.jump(800)
-    }
+onKeyDown("w", () => {
+    player.move(0, -400)
+})
+
+onKeyDown("s", () => {
+    player.move(0, 400)
 })
 
 onKeyDown("d", () => {
-    guy.move(400, 0)
+    player.move(400, 0)
 })
 
 onKeyDown("a", () => {
-    guy.move(-400, 0)
+    player.move(-400, 0)
 })
 
 let canShoot = 1
@@ -51,19 +91,17 @@ onMouseDown(() => {
         canShoot = 0;
         const bullet = add([
             sprite("bullet"),
+            origin("center"),
+            play("gunshot"),
             area(),
-            pos(guy.pos.x + 30, guy.pos.y + 30),
-            rotate(crosshair.pos.angle(guy.pos)),
-            move(crosshair.pos.angle(guy.pos), 3000),
+            pos(m4.pos.x, m4.pos.y),
+            rotate(crosshair.pos.angle(m4.pos)),
+            move(crosshair.pos.angle(m4.pos), 3000),
             cleanup(),
         ])
         wait(0.1, () => canShoot = 1)
     }
 })
-
-//onUpdate(() => {
-//    bullet.move(crosshair.pos.angle(guy.pos), 1000)
-//})
 
 mouseWorldPos.x, mouseWorldPos.y
 
@@ -73,6 +111,32 @@ add([
     outline(4),
     area(),
     solid(),
-    color(0, 200, 0),
+    color(100, 100, 100),
 ])
 
+add([
+    rect(width(), 48),
+    pos(0, -10),
+    outline(4),
+    area(),
+    solid(),
+    color(100, 100, 100),
+])
+
+add([
+    rect(48, height()),
+    pos(-5, 0),
+    outline(4),
+    area(),
+    solid(),
+    color(100, 100, 100),
+])
+
+add([
+    rect(48, height()),
+    pos(width() - 40, 0),
+    outline(4),
+    area(),
+    solid(),
+    color(100, 100, 100),
+])
